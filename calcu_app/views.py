921da238b36b53fdf62981b02e6aa9a32dee6861
kamakiri1225 <kusumuratakuya@yahoo.co.jp>
2020-12-01 20:33:41 +0900

    first commit

--- calcu_app/views.py
+++ calcu_app/views.py
@@ -23,65 +23,70 @@ def calcu(requset):
     return render(requset, 'calcu/calcu.html' , params)
 
 def result(requset):
-    params = {
-        'title': '計算実行プログラム',
-        'msg' : 'ここからが計算できます。',
-        'param_title' : 'パラメータ設定',
-        'setup' : SetupForm()
-    }
 
     nx = requset.POST['nx']
     xmax = requset.POST['xmax']
     c = requset.POST['c']
     alpha = requset.POST['alpha']
 
+
+    params = {
+        'title': '計算実行プログラム',
+        'msg' : 'ここからが計算できます。',
+        'param_title' : 'パラメータ設定',
+        'setup' : SetupForm(),
+        'nx':nx,
+        'xmax':xmax,
+        'c':c,
+        'alpha':alpha
+    }
+
+
     params['title'] = '計算結果'
     params['setup'] = SetupForm(requset.POST)
 
     AdvectionEquation(nx, xmax ,c , alpha)
 
-    setup ={
-        'nx': 'nx',
-        'xmax':'xmax',
-        'c': 'c',
-        'alpha': 'alpha'
-    }
+    # setup ={
+    #     'nx': 'nx',
+    #     'xmax':'xmax',
+    #     'c': 'c',
+    #     'alpha': 'alpha'
+    # }
     
-    return render(requset, 'calcu/result.html', params, setup)
+    return render(requset, 'calcu/result.html', params)
 
 import numpy as np
 import matplotlib.pyplot as plt
- 
+import matplotlib.animation as animation
+
 def AdvectionEquation(nx, xmax ,c , alpha):
-    nx = nx
-    xmax = xmax
-    dx = xmax / (nx-1)
-    nt = 25
-    c = c
-    alpha = alpha
-    dt = alpha * (dx/c) 
+    nx = int(nx)
+    xmax = int(xmax)
+    dx = float(xmax) / (float(nx)-1)
+    nt = 100
+    c = float(c)
+    alpha = float(alpha)
+    dt = 0.25 #alpha * (dx/c) 
     
     x = np.linspace(0,2,nx)
     
     un = []
     u = np.ones(nx)
-    u[int(.5 / dx):int(1 / dx + 1)] = 2
+    u[int(.5 / dx+1):int(1 / dx + 25/dx)] = 2
     
-    
+    fig = plt.figure(figsize=(8,4))
+    ims=[]
     for n in range(nt): 
         un = u.copy()
         for i in range(1, nx):
             u[i] = un[i] - c * dt / dx * (un[i] - un[i-1])
+        if (nt%1==0):
+            im = plt.plot(x,u, "r")
+            ims.append(im)
     
-    u_final = u.copy()
-    
-    u_initial  = np.ones(nx)
-    u_initial[int(.5 / dx):int(1 / dx + 1)] = 2
-    
-    plt.plot(x,u_initial,label = 'initial')
-    plt.plot(x,u_final, label = 'final')
-    plt.legend()
     plt.grid()
-    # plt.show()
-    plt.savefig('figure01.jpg')
-    plt.close()
+    plt.xlabel("x")
+    plt.ylabel("u(m/s)")   
+    anim = animation.ArtistAnimation(fig, ims)
+    anim.save('movie_reesult.gif', fps=5)
